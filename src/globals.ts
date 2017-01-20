e12fd6e23c240de01f15150e450136382cf0a35e
danzel <danzel@localhost.geek.nz>
2017-01-21 12:36:57 +1300

    smaller player

--- src/globals.ts
+++ src/globals.ts
@@ -2,7 +2,7 @@ export const ScreenWidth = 1920;
 export const ScreenHeight = 1080;
 export const DebugRender = true;
 
-export const PlayerRadius = 16;
+export const PlayerRadius = 10;
 export const ShotRadius = 3;
 
 export const ShotAwayDist = 30;
