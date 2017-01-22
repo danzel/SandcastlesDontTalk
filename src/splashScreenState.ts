b76d453dc5f9f8aa1643835758e5f03768b3e279
danzel <danzel@localhost.geek.nz>
2017-01-22 16:43:06 +1300

    Splash screen bouncing shots

--- src/splashScreenState.ts
+++ src/splashScreenState.ts
@@ -9,7 +9,30 @@ export default class LoadingState extends Phaser.State {
 
 	create() {
 
+		this.physics.startSystem(Phaser.Physics.P2JS);
+		this.physics.p2.restitution = 1;
+		this.physics.p2.friction = 0;
+		this.physics.p2.setImpactEvents(true);
+
 		this.add.sprite(0, 0, 'SplashScreen');
+
+
+		for (let i = 0; i < 200; i++) {
+			let shot = this.game.add.sprite(Math.random() * Globals.ScreenWidth, Math.random() * Globals.ScreenHeight, 'shot_' + Math.floor(Math.random() * 5));
+			shot.blendMode = PIXI.blendModes.ADD;
+			shot.scale.set(3 * Globals.ShotRadius / 136);
+
+			this.game.physics.p2.enable(shot);
+			let shotBody = <Phaser.Physics.P2.Body>shot.body;
+			shotBody.setCircle(Globals.ShotRadius);
+			shotBody.collideWorldBounds = true;
+			shotBody.moveRight((Math.random() * 2 - 1) * Globals.ShotSpeed);
+			shotBody.moveDown((Math.random() * 2 - 1) * Globals.ShotSpeed);
+			shotBody.damping = 0;
+
+			shotBody.angle = Math.random() * 360;
+		}
+
 		this.add.sprite(0, 0, 'Title');
 
 		this.padsText = this.add.text(this.world.centerX, this.world.height - 300, 'TODO', { font: '50px ' + Globals.FontName, fill: '#dddddd', align: 'center' });
